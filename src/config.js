12921291a236eaf97e1a5bfaad11ad4b7b10eb35
jeanramirezyagual <jean.ramirezyagual4910@upse.edu.ec>
2026-07-19 18:04:58 -0500

    listo

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 config();
 
-export const BD_HOST = process.env.BD_HOST || 'bwvoyynxz0cnet5rx2mk-mysql.services.clever-cloud.com';
-export const BD_DATABASE = process.env.BD_DATABASE || 'bwvoyynxz0cnet5rx2mk';
-export const BD_USER = process.env.BD_USER || 'uu0rwggf4nond0xd';
-export const BD_PASSWORD = process.env.BD_PASSWORD || 'Q10UsMo6336QBuwSUlF5';
+export const BD_HOST = process.env.BD_HOST;
+export const BD_DATABASE = process.env.BD_DATABASE;
+export const BD_USER = process.env.BD_USER;
+export const BD_PASSWORD = process.env.BD_PASSWORD;
 export const BD_PORT = process.env.BD_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
